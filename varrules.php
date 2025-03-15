a9612976d4573324a9d3048e5210f368a02198fc
BOLANKHAN <bolankhan9@gmail.com>
2025-03-15 08:06:59 -0700

    Add files via upload

---
+++ varrules.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Example single danger button -->
+ <h1>More About PHP</h1>
+ <h3>Rules of Creating varaible in PHP</h3>
+ <ul>
+  <li>Start with a $ sign</li>
+  <li>Cannot start with a Number</li>
+  <li>Must Start with a letter or an underscore charcter</li>
+  <li>Can Only contain alphanumeric characters and underscore</li>
+  <li>Variables in PhP are Case Sensitive. like $bolan and $Bolan are different</li>
+</ul>
+    
+</body>
+</html>
